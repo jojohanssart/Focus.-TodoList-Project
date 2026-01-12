b4d25c329d69f73d36156af1e4bf0b7b26e9fbf3
Josiah Hans <jojohanss.art@gmail.com>
2026-01-12 10:43:28 +0700

    Add and changed the message for each active filter tab

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,7 +18,7 @@ const saveData = () => {
 }
 
 
-   
+
 
 // Add function
 const addTodo = () => {
@@ -31,11 +31,11 @@ const addTodo = () => {
         id: Date.now()
     });
     renderTodoList();
-    
+
     console.log(todoList);
     inputElement.value = '';
     saveData();
-    
+
 }
 
 let visibleList;
@@ -46,7 +46,7 @@ const renderTodoList = () => {
 
     updateFilterButtons();
     visibleList.forEach((todo) => {
-    const html = `
+        const html = `
         <div class="todo-item">
         <input type="checkbox"
         onclick="toggleCheckbox(${todo.id})"
@@ -59,48 +59,49 @@ const renderTodoList = () => {
         todoHTML += html;
     });
 
-    
+
     todoDisplay.innerHTML = todoHTML;
     updateCounter();
 
     if (todoList.length === 0) {
         taskCounterElement.innerHTML =
-        'What needs to be done today?';
+            `Nothing here yet. Let's make today's count!`;
     } else if (completedTask > 0) {
         taskCounterElement.innerHTML =
-        `Great job! you have completed ${completedTask} tasks.`
+            `${completedTask} tasks done. Nice work!`
     } else if (todoList.length > 0 && completedTask === 0) {
         taskCounterElement.innerHTML =
-        `Ready to check off that first box?`
-    } 
-    
-    document.querySelector('.all-filter-btn').innerText = `All (${todoList.length})`;
-    document.querySelector('.active-filter-btn').innerText = `Active (${todoList.filter(todo => !todo.isChecked).length})`;
-    document.querySelector('.completed-filter-btn').innerText = `Completed (${todoList.filter(todo => todo.isChecked).length})`;
-    
-    if (todoList.length === 0) {
-    let html = '';
-    clearAllButtonElement.innerHTML = html;
-    } else {html = `
-    <button class="clear-btn" onclick="clearTodo()">Clear All</button> `;
-    clearAllButtonElement.innerHTML = html;
+            `Ready to check off that first box?`
     }
-    
+
+    document.querySelector('.all-filter-btn').innerText = `All (${todoList.length})`;
+    document.querySelector('.active-filter-btn').innerText = `To Do (${todoList.filter(todo => !todo.isChecked).length})`;
+    document.querySelector('.completed-filter-btn').innerText = `Done (${todoList.filter(todo => todo.isChecked).length})`;
+
+    if (todoList.length === 0) {
+        let html = '';
+        clearAllButtonElement.innerHTML = html;
+    } else {
+        html = `
+    <button class="clear-btn" onclick="clearTodo()">Clear All</button> `;
+        clearAllButtonElement.innerHTML = html;
+    }
+
 }
 
 // Delete function
 const deleteTodo = (id) => {
-   const indexToDelete = todoList.findIndex((todo) => todo.id === id);
+    const indexToDelete = todoList.findIndex((todo) => todo.id === id);
     if (indexToDelete !== -1) {
-    todoList.splice(indexToDelete, 1);
-    renderTodoList();
-    saveData();
+        todoList.splice(indexToDelete, 1);
+        renderTodoList();
+        saveData();
     }
 }
 
 
 // Clear all function
-const clearTodo = () => { 
+const clearTodo = () => {
     if (!confirm(`Are you sure you want to clear the whole list? You can't undo this!`)) {
         return
     }
@@ -115,12 +116,12 @@ const clearTodo = () => {
 // Checkbox function
 const toggleCheckbox = (id) => {
     const index = todoList.findIndex((todo) => todo.id === id);
-    
+
     if (index !== -1) {
-    const todo = todoList[index];
-    todo.isChecked = !todo.isChecked;
-    renderTodoList();
-    saveData();
+        const todo = todoList[index];
+        todo.isChecked = !todo.isChecked;
+        renderTodoList();
+        saveData();
     }
 }
 
@@ -167,15 +168,15 @@ const updateFilterButtons = () => {
         btn.classList.remove('.active-filter');
     });
     if (currentFilter === 'all') {
-            visibleList = todoList;
-            document.querySelector('.all-filter-btn').classList.add('active-filter');
-        } else if (currentFilter === 'active') {
-            visibleList = todoList.filter(todo => !todo.isChecked);
-            document.querySelector('.active-filter-btn').classList.add('active-filter');
-        } else if (currentFilter === 'completed') {
-            visibleList = todoList.filter(todo => todo.isChecked);
-            document.querySelector('.completed-filter-btn').classList.add('active-filter');
-        }
+        visibleList = todoList;
+        document.querySelector('.all-filter-btn').classList.add('active-filter');
+    } else if (currentFilter === 'active') {
+        visibleList = todoList.filter(todo => !todo.isChecked);
+        document.querySelector('.active-filter-btn').classList.add('active-filter');
+    } else if (currentFilter === 'completed') {
+        visibleList = todoList.filter(todo => todo.isChecked);
+        document.querySelector('.completed-filter-btn').classList.add('active-filter');
+    }
 }
 
 
